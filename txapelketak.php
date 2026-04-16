f8596f55f2a19bdfecd1280e5e4b3d1fc2c5a956
Oier Maiza <oie_mai_uga@goierrieskola.org>
2026-04-16 12:22:51 +0200

    Txapelketen filtroa eginda

--- txapelketak.php
+++ txapelketak.php
@@ -18,35 +18,56 @@
     include_once "navbar.php";
     include_once "konexioa.php";
 
-    $stmt = $pdo->query("Select * from txapelketak");
+    $egoera = $_GET["egoera"] ?? "";
+    $probintzia = $_GET["probintzia"] ?? "";
+
+    $where = "1=1";
+    if ($egoera != "") {
+        $where .= " AND egoera='$egoera'";
+    }
+    if ($probintzia != "") {
+        $where .= " AND probintzia='$probintzia'";
+    }
+
+    $stmt = $pdo->query("SELECT * FROM txapelketak WHERE $where");
     ?>
 
     <section class="txapelketak">
         <h1>Txapelketa Guztiak</h1>
         <p id="azalpena">Hemen Gipuzkoako mus-eko txapelketa guztiak ikus ditzazkezu!</p>
-        <div class="filtroak">
+
+        <form class="filtroak" action="txapelketak.php" method="get">
             <div class="filtro_barra">
-                <select>
-                    <option>Guztiak</option>
-                    <option>Amaituta</option>
-                    <option>Izen ematen</option>
-                    <option>Jolasten</option>
+                <select name="egoera">
+                    <option value="">-- Egoera --</option>
+                    <option value="Amaituta" <?= $egoera === "Amaituta" ? "selected" : ""; ?>>Amaituta</option>
+                    <option value="Izen ematen" <?= $egoera === "Izen ematen" ? "selected" : ""; ?>>Izen ematen</option>
+                    <option value="Jolasten" <?= $egoera === "Jolasten" ? "selected" : ""; ?>>Jolasten</option>
                 </select>
             </div>
             <div class="filtro_barra">
-                <select>
-                    <option>Guztiak</option>
-                    <option>Gipuzkoa</option>
-                    <option>Bizkaia</option>
-                    <option>Araba</option>
+                <select name="probintzia">
+                    <option value="">-- Probintzia --</option>
+                    <option value="Gipuzkoa" <?= $probintzia === "Gipuzkoa" ? "selected" : ""; ?>>Gipuzkoa</option>
+                    <option value="Bizkaia" <?= $probintzia === "Bizkaia" ? "selected" : ""; ?>>Bizkaia</option>
+                    <option value="Araba" <?= $probintzia === "Araba" ? "selected" : ""; ?>>Araba</option>
                 </select>
             </div>
-        </div>
+        </form>
+
         <?php include_once("txapelketa_kaxa.php"); ?>
     </section>
 
-
     <script src="https://code.jquery.com/jquery-4.0.0.js"
         integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script>
-    
+
+    <script>
+        $("select").change(function () {
+            $("form").submit();
+        });
+    </script>
+
     <?php include_once("footer.php"); ?>
+</body>
+
+</html>
